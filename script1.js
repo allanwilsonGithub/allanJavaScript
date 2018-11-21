125d0dc0a9c5f1d2a424ab579c3e57f31e9fa70a
Allan Wilson <allan@allanwilson.net>
2018-11-21 23:57:30 +0200

    Started learning arrays

--- script1.js
+++ script1.js
@@ -1,1 +1,22 @@
 console.log("https://www.udemy.com/the-complete-javascript-course")
+
+/*******************************
+* Arrays
+*/
+
+var names = ['John', 'Mark', 'Jane'];
+var years = new Array(1990, 1969, 1948);
+
+// 0 based. The first element is 0 index
+
+console.log(names[0]);
+console.log(names.length);
+
+names[1] = 'Ben';
+console.log(names);
+
+
+// Mutate array data
+names[names.length] = 'Tom';
+names[5] = 'Mary';
+console.log(names);
