c758153966f66af4ffb0d302744fd4a76ea1529d
Allan Wilson <allan@allanwilson.net>
2018-11-22 01:53:48 +0200

    More on arrays

--- script1.js
+++ script1.js
@@ -20,3 +20,26 @@ console.log(names);
 names[names.length] = 'Tom';
 names[5] = 'Mary';
 console.log(names);
+
+// Different data types
+var john = ['John', 'Smith', 1990, 'teacher', false];
+
+john.push('blue');
+john.unshift('Mr');
+console.log(john);
+
+john.pop();
+john.pop();
+console.log(john);
+
+john.shift();
+console.log(john);
+
+var john = ['John', 'Smith', 1990, 'teacher', false];
+john.push('blue');
+john.unshift('Mr');
+console.log(john);
+
+console.log(john.indexOf(1990));
+var isInArray = john.indexOf('teacher') === -1 ? 'Not in array' : 'In array'
+console.log(isInArray);
