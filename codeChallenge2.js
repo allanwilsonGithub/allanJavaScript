c7b23065eec4c72ed5278d01890bc55fa922fbdc
Allan Wilson <allan@allanwilson.net>
2018-11-23 00:49:28 +0200

    Antti's comments taken into account

--- codeChallenge2.js
+++ codeChallenge2.js
@@ -1,6 +1,6 @@
-var amounts = [124, 48, 268];
+const amounts = [124, 48, 268];
 
-function addTip(sum){
+const addTip = sum => {
     if (sum > 0 && sum < 50){
         return sum*1.2;
     } else if (sum => 50 && sum < 200) {
@@ -8,21 +8,37 @@ function addTip(sum){
     } else if (sum >= 200) {
         return sum*1.1;
     } else {
-        return 0
+        return 0;
     }
 }
 
-for (i in amounts){
-    var withTip = addTip((amounts[i]));
-    console.log(i + ": " + withTip);
+const tips = sum => {
+    if (sum > 0 && sum < 50){
+        return sum*0.2;
+    } else if (sum => 50 && sum < 200) {
+        return sum*0.15;   
+    } else if (sum >= 200) {
+        return sum*0.1;
+    } else {
+        return 0;
+    }
 }
+
+let tipsArray = amounts.map(a => tips(a));
+console.log("Tips: " + tipsArray)
+
+let finalBillsArray = amounts.map(a => addTip(a));
+console.log("Final bills: " + finalBillsArray)
+
+
+
 
 //let , map
 let newArray = amounts.map(a => addTip(a));
 console.log(newArray)
 
-//filter
 
+//filter
 function showIt(whatever){
     console.log(whatever);
 }
@@ -30,4 +46,3 @@ function showIt(whatever){
 // All dates after 2000
 let dates = [1999, 2012, 2019, 2023]
 dates.filter(a => a =>2000).map(a => showIt(a))
-
