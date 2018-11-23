ea0c8b823082f199b629a94fcf0da6f2d2333d0c
Allan Wilson <allan@allanwilson.net>
2018-11-23 23:17:22 +0200

    removed bulky code

--- codeChallenge2.js
+++ codeChallenge2.js
@@ -1,17 +1,5 @@
 const amounts = [124, 48, 268];
 
-const addTip = sum => {
-    if (sum > 0 && sum < 50){
-        return sum*1.2;
-    } else if (sum => 50 && sum < 200) {
-        return sum*1.15;   
-    } else if (sum >= 200) {
-        return sum*1.1;
-    } else {
-        return 0;
-    }
-}
-
 const tips = sum => {
     if (sum > 0 && sum < 50){
         return sum*0.2;
@@ -27,14 +15,12 @@ const tips = sum => {
 let tipsArray = amounts.map(a => tips(a));
 console.log("Tips: " + tipsArray)
 
-let finalBillsArray = amounts.map(a => addTip(a));
+let finalBillsArray = amounts.map(a => a + tips(a));
 console.log("Final bills: " + finalBillsArray)
 
 
-
-
 //let , map
-let newArray = amounts.map(a => addTip(a));
+let newArray = amounts.map(a => a + tips(a));
 console.log(newArray)
 
 
