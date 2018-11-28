276a7d6351d8d7cb59de2e9f04c9860b3684c0b7
Allan Wilson <allan@allanwilson.net>
2018-11-28 22:56:21 +0200

    challenge 4

--- script2.js
+++ script2.js
@@ -1,6 +1,4 @@
-/********************
-* Objects and Properties
-*/
+/*
 
 var john = {
     firstname: 'John',
@@ -16,3 +14,42 @@ console.log(john.firstname); //dot notation
 console.log(john['isMarried']); //brackets notation
 john.calcAge();
 console.log(john);
+
+*/
+
+var john = {
+    fullName : 'John Burrows',
+    mass : 80,
+    height : 1.8,
+    calcBMI: function(){
+        this.BMI = this.mass / (this.height * this.height);
+        return this.BMI;
+    }
+}
+
+var mark = {
+    fullName : 'Mark Reid',
+    mass : 80,
+    height : 1.8,
+    calcBMI: function(){
+        this.BMI = this.mass / (this.height * this.height);
+        return this.BMI;
+    }
+}
+                                
+john.calcBMI();
+console.log(john);
+johnsBMI = john.calcBMI();
+console.log(johnsBMI);
+mark.calcBMI();
+console.log(mark);
+marksBMI = mark.calcBMI();
+console.log(marksBMI);
+
+if (johnsBMI > marksBMI) {
+    console.log("John is a fat sod!")
+} else if (marksBMI > johnsBMI) {
+    console.log("Mark could do with less cake!")
+} else {
+    console.log("Both seem to be as HUGE as each other!")
+}
