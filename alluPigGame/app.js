e4ca4bf22ea4cfbe91811d84a3c1e18501211f62
Allan Wilson <allan@allanwilson.net>
2018-12-07 23:57:42 +0200

    Game coming along well

--- alluPigGame/app.js
+++ alluPigGame/app.js
@@ -9,19 +9,13 @@ GAME RULES:
 
 */
 
-var scores, roundScore;
+var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
-console.log(dice);
 
-console.log({scores,roundScore,activePlayer,dice})
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
-//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 
 var x = document.querySelector('#score-0').textContent;
 console.log({x});
@@ -30,6 +24,40 @@ function btn(){
     console.log('Clicked baby!')
 }
 
+document.getElementById('score-0').textContent = 0;
+document.getElementById('score-1').textContent = 0;
+document.getElementById('current-0').textContent = 0;
+document.getElementById('current-1').textContent = 0;
 
 document.querySelector('.dice').style.display = 'none';
-document.querySelector('.btn-roll').addEventListener('click', btn);
+//document.querySelector('.btn-roll').addEventListener('click', btn);
+document.querySelector('.btn-roll').addEventListener('click', function(){
+
+    // 1. Random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    // 2. Display the result
+    var diceDOM = document.querySelector('.dice');
+    document.querySelector('.dice').style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png'
+
+    // 3. Update the round score IF the rolled number is NOT 1
+    if (dice !== 1) {
+        roundScore += dice;
+        document.getElementById('current-' + activePlayer).textContent = roundScore;
+        document.querySelector('.dice').style.display = 'block';
+    } else {
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+        document.getElementById('current-0').textContent = roundScore;
+        document.getElementById('current-1').textContent = roundScore;
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+        document.querySelector('.dice').style.display = 'none';
+    }
+
+});
+
+
+//document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
